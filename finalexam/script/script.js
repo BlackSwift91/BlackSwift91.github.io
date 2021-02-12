4e5672963002ea31beda84e1e982c92d1c649cad
Andrey Arkhipov <blackswift91@gmail.com>
2021-02-12 14:11:12 +0200

    Add files via upload

--- finalexam/script/script.js
+++ finalexam/script/script.js
@@ -1,1 +1,125 @@
-const svg=document.getElementById("logo"),burger=document.getElementById("logo");let navList=document.getElementById("navList"),scrollToElem=document.getElementById("navList");navList.addEventListener("click",(e=>{let t=e.target;t=t.parentElement,t=t.dataset.scroll;let n=document.getElementById(t).getBoundingClientRect();window.screen.width<767?window.scrollBy({top:n.y-73,left:0,behavior:"smooth"}):window.scrollBy({top:n.y-102,left:0,behavior:"smooth"})})),window.screen.width<767&&(svg.setAttribute("width","35,698"),svg.setAttribute("height","48")),window.onscroll=function(){let e=document.getElementById("header");0!==window.pageYOffset?e.classList.add("header_scrolled"):e.classList.remove("header_scrolled")},burger.addEventListener("click",(()=>{let e=document.getElementById("navList");window.screen.width<767&&e.classList.toggle("burgerMenu")})),window.addEventListener("resize",(function(){window.screen.width<767?(svg.setAttribute("width","35,698"),svg.setAttribute("height","48")):(svg.setAttribute("width","53.547"),svg.setAttribute("height","72"))}));
+const svg = document.getElementById('logo');
+const burger = document.getElementById('logo');
+let navList = document.getElementById('navList');
+let scrollToElem = document.getElementById('navList');
+// const glide = document.querySelector('.glide');
+
+let conatactMobile = document.getElementById('contactInfo');
+console.log(conatactMobile);
+
+conatactMobile.addEventListener('click', (event) => {
+    let elem = event.target;
+    while (!elem.classList.contains("contactLink")) {
+        elem = elem.parentElement;
+        console.log(elem)
+    }
+    elem.classList.toggle('contactLinkMobile');
+
+    console.log(elem);
+
+});
+
+
+
+navList.addEventListener('click', (event) => {
+    let elem = event.target;
+    elem = elem.parentElement;
+    elem = elem.dataset.scroll;
+    let scrollToElem = document.getElementById(elem);
+
+    if (scrollToElem === 'home') {
+        indow.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        });
+        return;
+    }
+
+    let coord = scrollToElem.getBoundingClientRect();
+
+    if (window.screen.width < 767) {
+        window.scrollBy({
+            top: coord.y - 73, // could be negative value
+            left: 0,
+            behavior: 'smooth'
+        });
+    } else {
+        window.scrollBy({
+            top: coord.y - 102, // could be negative value
+            left: 0,
+            behavior: 'smooth'
+        });
+    }
+
+
+    // console.log(coord.y);
+    // elmnt.scrollIntoView({ block: "start", behavior: "smooth" });
+
+    // window.scrollBy(0, 130);
+});
+
+
+
+
+if (window.screen.width < 767) {
+    svg.setAttribute('width', "35,698");
+    svg.setAttribute('height', "48");
+
+}
+
+
+window.onscroll = function() {
+    let header = document.getElementById('header');
+    let pageOffsetX = window.pageYOffset;
+    if (pageOffsetX !== 0) {
+        header.classList.add('header_scrolled')
+    } else {
+        header.classList.remove('header_scrolled')
+    }
+};
+
+burger.addEventListener('click', () => {
+    let navList = document.getElementById('navList');
+    if (window.screen.width < 767) {
+        navList.classList.toggle('burgerMenu');
+    }
+});
+
+
+
+window.addEventListener('resize', function() {
+
+    if (window.screen.width < 767) {
+        svg.setAttribute('width', "35,698")
+        svg.setAttribute('height', "48")
+
+    } else {
+        svg.setAttribute('width', "53.547")
+        svg.setAttribute('height', "72")
+
+    }
+
+
+});
+
+
+
+
+
+
+// window.onresize = () => {
+//     let timer;
+//     let anyTime = 300;
+
+//     if (timer !== undefined) {
+//         clearTimeout(timer);
+//     }
+//     else {
+//         timer = setTimeout(() => {
+//             console.log(2)
+//         }, anyTime);
+//     }
+
+
+// }
